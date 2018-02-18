19043aaf32cac0f27de9a74dcd2e03df83775580
Jerry Holmes <jholmes@pagedragon.com>
2018-02-18 13:41:24 +0000

    No longer caching texture objects

--- src/textures.js
+++ src/textures.js
@@ -5,7 +5,6 @@ import _ from "lodash";
 let Textures = class {
     constructor(app) {
         this.app = app;
-        this.texturesById = {};
         this.getTexture = this.getTexture.bind(this);
        
     }
@@ -13,31 +12,18 @@ let Textures = class {
     getTexture(path) {
         
         return new Promise((resolve, reject) => {
-            // DO WE ALREADY HAVE THIS TEXTURE?
-            
-            let texture = this.texturesById[path];
-            
-            if(texture) {
-                resolve(texture);
-                return; 
-            }
             
             // CREATE TEXTURE
             
+            let texture;
             texture = new BABYLON.Texture(path,this.app.scene,false,true,BABYLON.Texture.NEAREST_SAMPLINGMODE,()=>{
                 resolve(texture);
             });
-            this.texturesById[path] = texture;
-            
         });
     }
     
     dispose() {
-        _.forOwn(this.texturesById, (value) => {
-            console.log("Disposing Texture:",value);
-            value.dispose();
-        } );
-        this.texturesById = {};
+        
     }
 };
 export default Textures;
